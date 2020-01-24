435f1b89bff301c35ad8ec4af4d0bb6fb0ce7500
dprozz <dmitri.proskuriakov@gmail.com>
2020-01-24 21:19:36 +0300

    new movements

--- main.py
+++ main.py
@@ -8,25 +8,29 @@ BG_COLOR = (249, 166, 2)
 START_X = 500
 START_Y = 400
 
+
 class agent():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
 
 class guard():
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode(SIZE)
     screen.fill(CL)
     pygame.display.update()
-    
+
     player1 = guard(START_X, START_Y)
     # player2= guard(100, 200)
-    
+    left, right, up, down = False, False, False, False
+
     done = False
     while not done:
         for i in pygame.event.get():  # events
@@ -34,22 +38,33 @@ if __name__ == "__main__":
                 done = True
             elif i.type == pygame.KEYDOWN:
                 if i.key == pygame.K_w:
-                    print('up')
-                    player1.y -= 10
-
+                    up = True
                 elif i.key == pygame.K_s:
-                    print('down')
-                    player1.y += 10
-
+                    down = True
                 elif i.key == pygame.K_a:
-                    print('left')
-                    player1.x -= 10
-
+                    left = True
                 elif i.key == pygame.K_d:
-                    print('right')
-                    player1.x += 10
+                    right = True
+            elif i.type == pygame.KEYUP:
+                if i.key == pygame.K_w:
+                    up = False
+                elif i.key == pygame.K_s:
+                    down = False
+                elif i.key == pygame.K_a:
+                    left = False
+                elif i.key == pygame.K_d:
+                    right = False
 
-        
+        if right == True:
+            player1.x += 10
+        if up == True:
+            player1.y -= 10
+        if left == True:
+            player1.x -= 10
+        if down == True:
+            player1.y += 10
+
+        pygame.time.wait(1000 // 60)
         pygame.draw.circle(screen, BG_COLOR, (player1.x, player1.y), 10, 0)
         pygame.display.update()
 
