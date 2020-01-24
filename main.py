feb3e29f2da804229e7061e0483ecdb4c007e6df
pavTiger <pavTiger@gmail.com>
2020-01-24 21:58:19 +0300

    -

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import numpy as np
 import math
 
 SIZE = (1024, 768)
-CL = (33, 33, 33)
-BG_COLOR = (249, 166, 2)
+BG_COLOR = (100, 100, 100)
 START_X = 500
 START_Y = 400
 
@@ -23,9 +22,9 @@ class guard():
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode(SIZE)
-    screen.fill(CL)
+    screen.fill(BG_COLOR)
     pygame.display.update()
-    image = pygame.image.load(r'guard1.png')
+    image = pygame.image.load(r'guard2.png')
     
     player1 = guard(START_X, START_Y)
     # player2= guard(100, 200)
@@ -33,7 +32,7 @@ if __name__ == "__main__":
 
     done = False
     while not done:
-        screen.fill(CL)
+        screen.fill(BG_COLOR)
         for i in pygame.event.get():  # events
             if i.type == pygame.QUIT:
                 done = True
@@ -56,5 +55,6 @@ if __name__ == "__main__":
         
         screen.blit(image, (player1.x, player1.y))
         pygame.display.update()
+        print('hey')
 
     pygame.quit()
