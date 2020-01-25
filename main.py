dcd59bbaa579297c0210e438a4da36ee19cc6715
Pavel Artushkov <pavtiger@gmail.com>
2020-01-25 08:31:49 +0300

    ready for ghost generation

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 import numpy as np
 import math
+import datetime
 
 SIZE = (1024, 768)
 BG_COLOR = (50, 50, 50)
 START_X = 500
 START_Y = 400
-
+SS = 2  # Step Size
 
 class ghost():
     def __init__(self, x, y):
@@ -18,6 +19,7 @@ class guard():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.bulb = False
 
 
 if __name__ == "__main__":
@@ -30,12 +32,13 @@ if __name__ == "__main__":
     image_right = pygame.image.load(r'guard1.png')
     image_front = pygame.image.load(r'guard2.png')
     image_left = pygame.image.load(r'guard3.png')
-    ghost = pygame.image.load(r'ghost.jpg')
+    ghost = pygame.image.load(r'ghost2.png')
     image = image_front
 
     player1 = guard(START_X, START_Y)
     fps = 240
     left, right, up, down = False, False, False, False
+
 
     done = False
     while not done:
@@ -44,6 +47,12 @@ if __name__ == "__main__":
             if i.type == pygame.QUIT:
                 done = True
             elif i.type == pygame.KEYDOWN:
+                if i.key == pygame.K_SPACE:
+                    if player1.bulb:
+                        player1.bulb = False
+                    else:
+                        player1.bulb = True
+
                 if i.key == pygame.K_w:
                     up = True
                     image = image_back
@@ -67,14 +76,15 @@ if __name__ == "__main__":
                     right = False
 
         if right == True:
-            player1.x += 1
+            player1.x += SS
         if up == True:
-            player1.y -= 1
+            player1.y -= SS
         if left == True:
-            player1.x -= 1
+            player1.x -= SS
         if down == True:
-            player1.y += 1
+            player1.y += SS
 
+        # print(datetime.datetime.time()[1])
         pygame.time.wait(1000 // fps)
         screen.fill(BG_COLOR)
         screen.blit(image, (player1.x, player1.y))
